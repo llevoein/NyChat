3b8eb8b344172da63c6d372c6bb6487da600b6ed
llevoein <andrey.mozgo@becloud.by>
2023-04-10 17:31:52 +0300

    think about design

--- ui/static/js/player.js
+++ ui/static/js/player.js
@@ -57,92 +57,21 @@ muteIconContainer.addEventListener('click', () => {
         muteState = 'unmute';
     }
 });
-/*
-const showRangeProgress = (rangeInput) => {
-    if(rangeInput === seekSlider) audioPlayerContainer.style.setProperty('--seek-before-width', rangeInput.value / rangeInput.max * 100 + '%');
-    else audioPlayerContainer.style.setProperty('--volume-before-width', rangeInput.value / rangeInput.max * 100 + '%');
-}
-*/
-/*
-seekSlider.addEventListener('input', (e) => {
-    showRangeProgress(e.target);
-});*/
-/*
-volumeSlider.addEventListener('input', (e) => {
-    showRangeProgress(e.target);
-});/*
-
-
-
 
 
 /** Implementation of the functionality of the audio player */
 
 const audio = document.querySelector('audio');
-/*
-const durationContainer = document.getElementById('duration');
-const currentTimeContainer = document.getElementById('current-time');
-*/
+
 const outputContainer = document.getElementById('volume-output');
 let raf = null;
-/*
-const calculateTime = (secs) => {
-    const minutes = Math.floor(secs / 60);
-    const seconds = Math.floor(secs % 60);
-    const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
-    return `${minutes}:${returnedSeconds}`;
-}
 
-const displayDuration = () => {
-    durationContainer.textContent = calculateTime(audio.duration);
-}
-
-const setSliderMax = () => {
-    seekSlider.max = Math.floor(audio.duration);
-}
-
-const displayBufferedAmount = () => {
-    const bufferedAmount = Math.floor(audio.buffered.end(audio.buffered.length - 1));
-    audioPlayerContainer.style.setProperty('--buffered-width', `${(bufferedAmount / seekSlider.max) * 100}%`);
-}
-*/
 const whilePlaying = () => {
     /*seekSlider.value = Math.floor(audio.currentTime);*/
     /*currentTimeContainer.textContent = calculateTime(seekSlider.value);*/
     /*audioPlayerContainer.style.setProperty('--seek-before-width', `${seekSlider.value / seekSlider.max * 100}%`);*/
     raf = requestAnimationFrame(whilePlaying);
 }
-/*
-
-if (audio.readyState > 0) {
-    displayDuration();
-    setSliderMax();
-    displayBufferedAmount();
-} else {
-    audio.addEventListener('loadedmetadata', () => {
-        displayDuration();
-        setSliderMax();
-        displayBufferedAmount();
-    });
-}
-
-audio.addEventListener('progress', displayBufferedAmount);
-
-seekSlider.addEventListener('input', () => {
-    currentTimeContainer.textContent = calculateTime(seekSlider.value);
-    if(!audio.paused) {
-        cancelAnimationFrame(raf);
-    }
-});
-
-seekSlider.addEventListener('change', () => {
-    audio.currentTime = seekSlider.value;
-    if(!audio.paused) {
-        requestAnimationFrame(whilePlaying);
-    }
-});
-*/
-
 volumeSlider.addEventListener('input', (e) => {
     const value = e.target.value;
 
